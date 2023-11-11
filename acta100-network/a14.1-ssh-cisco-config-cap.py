1b12973df85abce45de532784e5ec39a25499b77
focaccio <greg@focaccio.net>
2023-11-11 10:40:51 -0800

    acta-code-a14

---
+++ acta100-network/a14.1-ssh-cisco-config-cap.py
@@ -0,0 +1,36 @@
+###############################################################################################
+#~~~PRE-CONDITIONS
+#username must be privi 15
+#term len 0
+###############################################################################################
+###############################################################################################
+#~~~VARIABLES-NOTES
+#
+###############################################################################################
+import subprocess
+# variables
+hostname = 'a.b.c.d'
+username = 'username'
+password = 'password'
+
+# ssh command to net device
+ssh_command = f"ssh -oKexAlgorithms=+diffie-hellman-group1-sha1 -c 3des-cbc {username}@{hostname} 'sh run'"
+
+try:
+    # Execute SSH command
+    result = subprocess.check_output(ssh_command, shell=True, text=True)
+
+    # Extracting sh_run from the result
+    sh_run = result.strip()
+
+    # Write sh_run to a local file
+    with open('network-config.txt', 'w') as file:
+        file.write(sh_run)
+
+    print(f"sh-run: {sh_run}")
+    print("Data written to network-config.txt")
+except subprocess.CalledProcessError as e:
+    print(f"Error: {e}")
+###############################################################################################
+#~~~areaf aggregated 20231111
+###############################################################################################
