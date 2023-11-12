7642e544cfd40583428d92bda056ef4cb56718f8
focaccio <greg@focaccio.net>
2023-11-12 15:51:11 -0800

    327

--- acta100-network/acta14.1-ssh-cisco-config-cap.py
+++ acta100-network/acta14.1-ssh-cisco-config-cap.py
@@ -18,11 +18,18 @@
 #
 ###############################################################################################
 #~~~acta14.alpha
+import datetime
 import subprocess
 # variables
-hostname = 'a.b.c.d'
+hostname = "a.b.c.d"
 username = 'username'
 password = 'password'
+
+#set-up
+current_datetime = datetime.datetime.now()
+date_string = current_datetime.strftime("%Y%m%d-%H%M%S")
+filename = f"{hostname}_config_{date_string}"
+
 # ssh command to net device
 ssh_command = f"ssh -oKexAlgorithms=+diffie-hellman-group1-sha1 -c 3des-cbc {username}@{hostname} 'sh run'"
 try:
@@ -33,10 +40,11 @@ try:
     sh_run = result.strip()
 
     # Write sh_run to a local file
-    with open('network-config.txt', 'w') as file:
+    with open(filename, 'w') as file:
         file.write(sh_run)
     print(f"sh-run: {sh_run}")
-    print("Data written to network-config.txt")
+    print("Data written to")
+    print(filename)
 except subprocess.CalledProcessError as e:
     print(f"Error: {e}")
 #~~~acta14.omega
